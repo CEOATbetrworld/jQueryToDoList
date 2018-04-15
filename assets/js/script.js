8be6bcfbde4dc3ca75c0165246c4fafeeb6ad274
DHRUV KUMAR <dkiphones620@gmail.com>
2018-04-15 11:44:32 +0530

    added remove and fadeOut

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,12 @@
 $("li").click(function () {
     $(this).toggleClass("done")
 })
+
+// Click on X to delete to-do
+
+$("span").click(function (event) {
+    event.stopPropagation();
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove()
+    });
+})
